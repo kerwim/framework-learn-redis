bb88147aab89ff9602ced6be22f685e1b3607202
mac <0924>
2024-10-11 23:20:55 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworklearnredis/FrameworkLearnRedisApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearnRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkLearnRedisApplication.class, args);
+    }
+
+}
